c37b7a358dbe7a03a302a5d69529646c0f9ad89e
Aryan Santosh Sawant <102072945+LaxmansAryan@users.noreply.github.com>
2023-06-04 16:15:58 +0530

    update 2
    - Created public views for displaying course information
    - Built a student registration system
    - Managed student enrollment onto courses
    - Rendered diverse content for course modules
    - Installed and configure Memcached
    - Cached content using the Django cache framework
    - Used the Memcached and Redis cache backends
    - Monitored Redis server in the Django administration site

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.list import ListView
@@ -12,6 +13,14 @@ from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from .forms import ModuleFormSet
 from .models import Course
 from .models import Module, Content
+from django.db.models import Count
+from .models import Subject
+from django.views.generic.detail import DetailView
+from students.forms import CourseEnrollForm
+from django.core.cache import cache
+
+
+
 
 
 class ManageCourseListView(ListView):
@@ -186,3 +195,43 @@ class ContentOrderView(CsrfExemptMixin,
                        module__course__owner=request.user) \
                        .update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+    def get(self, request, subject=None):
+        subjects = cache.get('all_subjects')
+        if not subjects:
+            subjects = Subject.objects.annotate(
+                            total_courses=Count('courses'))
+            cache.set('all_subjects', subjects)
+        all_courses = Course.objects.annotate(
+            total_modules=Count('modules'))
+        
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            key = f'subject_{subject.id}_courses'
+            courses = cache.get(key)
+            if not courses:
+                courses = all_courses.filter(subject=subject)
+                cache.set(key, courses)
+        else:
+            courses = cache.get('all_courses')
+            if not courses:
+                courses = all_courses
+                cache.set('all_courses', courses)
+
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject,
+                                        'courses': courses})
+    
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(
+                                    initial={'course':self.object})
+        return context
